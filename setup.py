7d8e91ce410bf1add9a21777afc0517198c11ced
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-01-27 19:00:10 +0100

    Change Name to ovhapi Set me as maintainer (not author)

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 setup(
-    name = "OvhApi",
+    name = "ovhapi",
     version = "1.0",
     description='Python module to manage Ovh API',
-    author='Guilhem Lettron',
-    author_email='guilhem.lettron@optiflows.com',
+    maintainer='Guilhem Lettron',
+    maintainer_email='guilhem.lettron@optiflows.com',
     url='https://github.com/optiflows/OvhApi',   
     packages = find_packages(),
 )
