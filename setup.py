1cb478349c2e125a4a2a33ec5af88c187c3dd462
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-01-27 18:36:52 +0100

    Fix after devpi advices

--- setup.py
+++ setup.py
@@ -2,5 +2,9 @@ from setuptools import setup, find_packages
 setup(
     name = "OvhApi",
     version = "1.0",
+    description='Python module to manage Ovh API',
+    author='Guilhem Lettron',
+    author_email='guilhem.lettron@optiflows.com',
+    url='https://github.com/optiflows/OvhApi',   
     packages = find_packages(),
 )
